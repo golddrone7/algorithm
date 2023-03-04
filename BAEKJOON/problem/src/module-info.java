1e5388326922899d5218a8071ac988fb27746dd8
golddrone7 <golddrone@naver.com>
2023-03-04 23:00:46 +0900

    첫번째 백준 문제

---
+++ BAEKJOON/problem/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author goldd
+ *
+ */
+module problem {
+}
---
+++ BAEKJOON/problem/src/problem/Baek1000.java
@@ -0,0 +1,20 @@
+package problem;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baek1000 {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			String nums[]=br.readLine().split(" ");
+			int a = Integer.parseInt(nums[0]);
+			int b = Integer.parseInt(nums[1]);
+			System.out.println(a+b);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
