fc3494e80a2571cffd274511088230dda955c2e9
golddrone7 <golddrone@naver.com>
2023-03-05 14:41:03 +0900

    백준 문제

--- BAEKJOON/problem/src/problem/Baek10430.java
+++ BAEKJOON/problem/src/problem/Baek10430.java
@@ -4,19 +4,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Baek1001 {
-
+public class Baek10430 {
 	public static void main(String[] args) {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		try {
-			String[] nums = br.readLine().split(" ");
+			String nums[] = br.readLine().split(" ");
 			int a = Integer.parseInt(nums[0]);
 			int b = Integer.parseInt(nums[1]);
-			System.out.println(a-b);
+			int c = Integer.parseInt(nums[2]);
+			
+			System.out.println((a+b)%c);
+			System.out.println(((a%c)+(b%c))%c);
+			System.out.println((a*b)%c);
+			System.out.println(((a%c)*(b%c))%c);	
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 	}
 }
---
+++ BAEKJOON/problem/src/problem/Baek1620.java
@@ -0,0 +1,46 @@
+package problem;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Baek1620 {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			String[] answer = br.readLine().split(" ");
+			int num = Integer.parseInt(answer[0]);
+			int result = Integer.parseInt(answer[1]);
+			
+			Map<Integer, String> dogam = new HashMap<>();
+			Map<String, Integer> dogam2 = new HashMap<>();
+			String[] results = new String[result];
+			
+			
+			for(int i=1; i<=num; i++) {
+				String s = br.readLine();
+				dogam.put(i, s);
+				dogam2.put(s, i);
+			}
+			
+			for(int i=0; i<result; i++) {
+				results[i] = br.readLine();
+			}
+			
+			for(int i=0; i<result; i++) {
+				try {
+					int index = Integer.parseInt(results[i]);
+					System.out.println(dogam.get(index));
+				} catch(NumberFormatException e){
+					System.out.println(dogam2.get(results[i]));
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
