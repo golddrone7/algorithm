06fe1840a37ba29c30de6eea9489679f1fe9a853
golddrone7 <95079688+golddrone7@users.noreply.github.com>
2023-03-06 18:45:32 +0900

    1343번 리팩토링

---
+++ BAEKJOON/problem/src/problem/Baek1343_V1.java
@@ -0,0 +1,25 @@
+package problem;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baek1343_V1 {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String answer = "";
+		try {
+			String s = br.readLine();
+			s = s.replaceAll("XXXX", "AAAA");
+			s = s.replaceAll("XX", "BB");
+			if(s.contains("X")) {
+				s = "-1";
+			}
+			System.out.println(s);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
