220abfec2a24e1cee7759965991a9febc61c65cf
golddrone7 <95079688+golddrone7@users.noreply.github.com>
2023-03-07 19:43:07 +0900

    백준 풀이

---
+++ BAEKJOON/problem/src/problem/Baek14916_V1.java
@@ -0,0 +1,21 @@
+package problem;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Baek14916_V1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] dp = new int[100001];
+		Arrays.fill(dp, Integer.MAX_VALUE);
+		dp[2] = dp[5] = 1;
+		dp[4] = 2;
+		for(int i=6; i<=N; i++) {	// 6일땐 4, 1
+			dp[i] = Math.min(dp[i-2], dp[i-5]) +1;
+		}
+		System.out.println(dp[N]==Integer.MAX_VALUE ? -1 : dp[N]);
+	}
+
+}
---
+++ BAEKJOON/problem/src/problem/Baek14916_V2.java
@@ -0,0 +1,11 @@
+package problem;
+
+public class Baek14916_V2 {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
