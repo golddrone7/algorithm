67dfdacf76873c21485b29ec68394135029b8613
golddrone7 <95079688+golddrone7@users.noreply.github.com>
2023-04-18 13:43:11 +0900

    [Bronze V] Title: A-B, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			String[] nums = br.readLine().split(" ");
+			int a = Integer.parseInt(nums[0]);
+			int b = Integer.parseInt(nums[1]);
+			System.out.println(a-b);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+}
