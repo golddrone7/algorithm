e9e030de355243404581b7dc7833dde28f8ec707
golddrone7 <95079688+golddrone7@users.noreply.github.com>
2023-04-26 10:51:19 +0900

    [Bronze V] Title: A/B, Time: 208 ms, Memory: 17768 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] arr = sc.nextLine().split(" ");
+        int a = Integer.parseInt(arr[0]);
+        int b = Integer.parseInt(arr[1]);
+        System.out.println(a /(double)b);
+    }
+}
