3e70d51233d0a638221b8968150fb454d156c779
golddrone7 <95079688+golddrone7@users.noreply.github.com>
2023-05-01 13:58:57 +0900

    [Bronze II] Title: 소수 찾기, Time: 136 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[] test = br.readLine().split(" ");
+        int answer = 0;
+        for (int i = 0; i < test.length; i++) {
+            int m = Integer.parseInt(test[i]);
+            int cnt = 0;
+            for (int j = 1; j <= m; j++) {
+                if(m%j==0) cnt++;
+                if(cnt>2) break;
+            }
+            if(cnt==2) answer++;
+        }
+        System.out.println(answer);
+
+    }
+}
