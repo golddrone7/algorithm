15f85cca76ed9af1ae99904fb2d8eff0e47bd488
golddrone7 <95079688+golddrone7@users.noreply.github.com>
2023-05-14 15:06:31 +0900

    [level 0] Title: 등수 매기기, Time: 0.03 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int[][] score) {
+        int[] answer=new int[score.length];
+		
+		for(int i=0; i<score.length; i++)
+			answer[i] = 1;	
+		for(int i=0; i<score.length; i++) {
+			for(int j=0; j<score.length; j++) {
+				if(score[i][0]+score[i][1]<score[j][0]+score[j][1])
+					answer[i]++;
+			}
+		}
+        return answer;
+    }
+}
