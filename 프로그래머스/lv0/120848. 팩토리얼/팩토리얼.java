0867e24e50ba42f0fbc0306006a72c7e3e424919
golddrone7 <95079688+golddrone7@users.noreply.github.com>
2023-05-14 15:08:27 +0900

    [level 0] Title: 팩토리얼, Time: 0.01 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int factorial=1;
+		for(int i=1; i<=10; i++) {
+			factorial*=i;
+			if(factorial>n) {
+				answer=i-1;
+				break;
+			}
+			if(factorial==n) {
+				answer=i;
+				break;
+			}
+		}
+        return answer;
+    }
+}
