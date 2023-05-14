a9dd67eec99055c817c92a7f2e45532cdb5a22b1
golddrone7 <95079688+golddrone7@users.noreply.github.com>
2023-05-14 15:08:22 +0900

    [level 0] Title: 모음 제거, Time: 0.22 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string) {
+        
+        my_string = my_string.replaceAll("a", "");
+        my_string = my_string.replaceAll("e", "");
+        my_string = my_string.replaceAll("o", "");
+        my_string = my_string.replaceAll("i", "");
+        my_string = my_string.replaceAll("u", "");
+        
+        String answer = my_string;
+        return answer;
+    }
+}
