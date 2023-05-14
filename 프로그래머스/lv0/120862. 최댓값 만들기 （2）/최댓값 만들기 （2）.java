05adcd11365a3198934662f8e685b7cef0f7f20f
golddrone7 <95079688+golddrone7@users.noreply.github.com>
2023-05-14 15:07:29 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.02 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int max = -2100000000;
+		
+		for(int i=0; i<numbers.length; i++) {
+			for(int j=0; j<numbers.length; j++) {
+				if(i==j)
+					continue;
+				if(max<numbers[i]*numbers[j]) {
+					max = numbers[i]*numbers[j];
+				}
+			}
+		}
+        return max;
+    }
+}
